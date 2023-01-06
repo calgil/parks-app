4ccb25ad3137eb1ad875eff01abaf71be5aef40d
calgil <clgilbert1105@gmail.com>
2023-01-06 16:01:50 -0700

    prep to get parks

--- src/fetch/parks/getParksByState.js
+++ src/fetch/parks/getParksByState.js
@@ -1,3 +1,15 @@
-export const getParksByState = (stateCode) => {
-  console.log("get parks fetch", stateCode);
+import { PARK_URL_BASE } from "../config";
+
+export const getParksByState = async (stateCode) => {
+  try {
+    const response = await fetch(
+      `${PARK_URL_BASE}${stateCode}&api_key=${
+        import.meta.env.VITE_PARKS_API_KEY
+      }`
+    );
+    const data = await response.json();
+    console.log("res", data);
+  } catch (error) {
+    console.error(error);
+  }
 };
